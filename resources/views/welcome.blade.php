28f93f1118682cfdfa9bd9ba9579f26a7e6f1f45
denitmpgith <denimytmp@gmail.com>
2023-03-10 01:41:19 +0700

    014110032023

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,78 @@
-@extends('template.main')
-
-@section('container')
-<section class="grid grid-cols-12">
-    <div class="container col-start-2 col-span-10 h-full bg-slate-700">
-    <h1 class="text-white">hi, ini adalah halaman home</h1>
+@include('template.header')
+@include('template.navbar')
+<body class="grid grid-cols-12 pt-16 ">
+    <section class="col-span-12 grid grid-cols-12 bg-slate-100">
+        <div class="col-span-12 flex justify-center items-center flex-col md:col-start-1 md:col-span-6 lg:col-start-1 lg:col-span-7 h-[750px] p-16">
+            <div class="flex justify-center items-center flex-col p-10">
+                <div class="w-[150] lg:w-[500px]">
+                    <p class="text-3xl font-bold text-cyan-500 uppercase">Platform Pencari Freelancer Unggul</p>
+                    <p class="text-lg">diantara freelancer-freelancer terbaik, dengan tidak mengorbankan freelancer yang lain</p>
+                </div>
+                <div class="w-[150] lg:w-[500px]">
+                    <li class="text-lg">bukan hanya untuk mencari Freelancer berbakat, tapi menghargai freelacer yang sudah berusaha membuat sesuatu untuk penyelenggara kontes</li>
+                    <li class="text-lg">Moto kami : Waktu lebih berharga dari uang</li>
+                </div>
+            </div>
+        </div>
+        <div class="col-span-12 flex justify-center items-center md:col-start-7 md:col-span-6 lg:col-start-8 lg:col-span-5">
+            <div class="w-[150] lg:h-[750px] flex justify-center items-center flex-col">
+                <img src="default/untitled.png" alt="">
+            </div>
+        </div>
+    </section>
+    <section class="col-span-12 grid grid-cols-12 ">
+        <div class="col-span-12 flex justify-center items-center md:col-start-1 md:col-span-6 lg:col-start-1 lg:col-span-5">
+            <div class="w-[200] lg:h-[500px] flex justify-center items-center"></div>
+        </div>
+        <div class="col-span-12 flex justify-center items-center flex-col md:col-start-7 md:col-span-6 lg:col-start-6 lg:col-span-7 h-[750px]">
+            <div class="flex justify-center items-center flex-col p-10">
+                <div class="w-[200] lg:w-[500px]">
+                    <p class="text-3xl font-bold text-cyan-500 uppercase">Bisnis anda Berharga per detik</p>
+                    <p class="text-lg">Logos, websites, packaging design and more. Our trusted designer community has helped thousands of businesses launch, grow, expand and rebrand with custom, professional design.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="col-span-12 grid grid-cols-12 bg-slate-100">
+        <div class="col-span-12 flex justify-center items-center flex-col md:col-start-1 md:col-span-6 lg:col-start-1 lg:col-span-7 h-[750px]">
+            <div class="flex justify-center items-center flex-col p-10">
+                <div class="w-[200] lg:w-[500px]">
+                    <p class="text-3xl font-bold text-cyan-500 uppercase">Platform Pencari Freelancer Unggul</p>
+                    <p class="text-lg">diantara freelancer-freelancer terbaik, dengan tidak mengorbankan freelancer yang lain</p>
+                </div>
+                <div class="w-[200] lg:w-[500px]">
+                    <li class="text-lg">bukan hanya untuk mencari Freelancer berbakat, tapi menghargai freelacer yang sudah berusaha membuat sesuatu untuk penyelenggara kontes</li>
+                    <li class="text-lg">Moto kami : Waktu lebih berharga dari uang</li>
+                </div>
+            </div>
+        </div>
+        <div class="col-span-12 flex justify-center items-center md:col-start-7 md:col-span-6 lg:col-start-8 lg:col-span-5">
+            <div class="w-[200] lg:h-[500px] flex justify-center items-center"></div>
+        </div>
+    </section>
+    <div class="fixed -z-20 h-screen w-screen opacity-10 flex justify-center items-center flex-col">
+        <img class="bg-repeat" src="default/world_map.png" alt="">
     </div>
-</section>
-@endsection
+</body>
+<footer class="col-span-12 rounded-lg grid grid-cols-12 bg-slate-200 mt-2 shadow">
+    <div class="col-start-3 col-span-8">
+        <div class="grid grid-cols-12 pt-3 gap-3">
+            <div class="col-span-3 h-20 text-center">
+                <p>about us</p>
+            </div>
+            <div class="col-span-3 h-20 text-center">
+                <p>about us</p>
+            </div>
+            <div class="col-span-3 h-20 text-center">
+                <p>about us</p>
+            </div>
+            <div class="col-span-3 h-20 text-center">
+                <p>about us</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-span-12 py-3 flex justify-center">
+        <small class="text-center">&copy; 2023 company Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></small>
+    </div>
+</footer>
+</html>
